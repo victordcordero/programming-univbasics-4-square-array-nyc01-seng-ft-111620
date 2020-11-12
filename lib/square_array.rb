ebdf48927bca5055a32ce027bc326c5b35ebc04f
Victor Cordero <victordcordero@gmail.com>
2020-11-12 18:05:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  count = 1
-  while index <= 5 do
+  count = 0
+  while index < 5 do
     puts (array*array)
-    index = index + 1
+    count += 1
 end
 end
